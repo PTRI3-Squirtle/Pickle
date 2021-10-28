6dcec0c45ad94fcb208ed72b9dc3ad500fff293c
RRosio <rosio_reyes@yahoo.com>
2021-10-27 22:14:15 -0700

    began package install, webpack and application structure setup

---
+++ src/frontend/app.tsx
@@ -0,0 +1,9 @@
+const App = (): JSX.Element => {
+    return (
+        <div>"PICKLE"
+          <img src='../assets/index.jpg' alt="It's a pickle!!!"></img>
+        </div>
+    );
+}
+
+export default App;
